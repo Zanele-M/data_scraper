02357cbd0c69370958099d78a1fb66e80d79cb6f
Zanele Sibanyoni <zs@winfuture.de>
2024-02-11 23:08:15 +0200

    Remove logging configuration from api/settings.py
    The logger configuration has been removed from the settings file in the API. This clean-up job simplifies the settings file, making it more manageable and easier

--- api/settings.py
+++ api/settings.py
@@ -86,21 +86,3 @@ REST_FRAMEWORK = {
         'rest_framework_api_key.permissions.HasAPIKey',
     )
 }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
