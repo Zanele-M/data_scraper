295ba22e9b474901f81e3500fea36e892068ae92
Zanele Sibanyoni <zs@winfuture.de>
2024-02-12 12:58:59 +0200

    Remove REST framework default renderer classes
    The default renderer classes set in REST framework settings were removed. This change is in line with the aim to use the Django default renderers. Consequently, this removal will affect how the api responds to requests.

--- api/settings.py
+++ api/settings.py
@@ -78,9 +78,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-}
