5f988e8e10ed2102aa553cae0e66ebbd4b040061
Zanele Sibanyoni <zs@winfuture.de>
2024-02-14 15:19:30 +0200

    Decrease MAX_ATTEMPTS in icon extraction

--- api/views/icon.py
+++ api/views/icon.py
@@ -21,7 +21,7 @@ from api.utils.html_content_parser import extract_html_element_attribute, downlo
 
 logger = logging.getLogger(__name__)
 
-MAX_ATTEMPTS = 50
+MAX_ATTEMPTS = 10
 
 #todo test for same id
 def extract_icon(url: str, search_term_instance: SearchTerm, program_name: str) -> HttpResponse | Response:
