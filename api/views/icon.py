1c947ea069a02c89de9d9de5d9ca706bfe6b51bb
Zanele Sibanyoni <zs@winfuture.de>
2024-02-19 08:19:15 +0200

    Add timing and logging for icon extraction process

--- api/views/icon.py
+++ api/views/icon.py
@@ -1,4 +1,5 @@
 import base64
+import time
 
 from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets, status
@@ -28,6 +29,7 @@ def extract_icon(url: str, search_term_instance: SearchTerm, program_name: str) 
     Extract the required meta attribute from the URL and respond with the image.
     """
 
+    start_time = time.time()
     if search_term_instance.attempts >= MAX_ATTEMPTS:
         return JsonResponse({'error': f"Maximum number of download attempts reached for {program_name}."},
                             status=status.HTTP_200_OK)
@@ -39,6 +41,8 @@ def extract_icon(url: str, search_term_instance: SearchTerm, program_name: str) 
     meta_search_criteria = {"name": "meta", "attrs": {"property": "og:image"}}
     meta_attribute = "content"
     meta_result = extract_html_element_attribute(url, meta_search_criteria, meta_attribute)
+    execution_time = time.time() - start_time
+    logger.info(f"Extract icon execution time for {program_name}: {execution_time} seconds.")
     if isinstance(meta_result, list) and meta_result and isinstance(meta_result[0], dict) and "error" in meta_result[0]:
         return JsonResponse({'error': f'Could not parse from the url: {url}'}, status=status.HTTP_200_OK)
     elif isinstance(meta_result, list) and not meta_result:
@@ -60,6 +64,7 @@ def search_icon(program_name: str, program_id: str) -> HttpResponse:
     """
     Perform a new search for the program and extract the required attribute.
     """
+    start_time = time.time()
     sites = [
         {'site': 'computerbase.de', 'inurl': 'downloads', 'url_pattern': 'https://www.computerbase.de/downloads/*'},
         {'site': 'uptodown.com', 'inurl': 'windows', 'url_pattern': 'https://.*\\.uptodown\\.com/windows'},
@@ -74,6 +79,8 @@ def search_icon(program_name: str, program_id: str) -> HttpResponse:
 
         search_term_instance, _ = SearchTerm.objects.get_or_create(term=f"{program_name} site:{site} inurl:{inurl}")
         if search_term_instance.attempts > MAX_ATTEMPTS:
+            execution_time = time.time() - start_time  # End timing
+            logger.info(f"Extract icon execution time for {program_name}: {execution_time} seconds.")
             return JsonResponse({'error': f"Maximum number of google search attempts reached for {program_name}"},
                                 status=status.HTTP_200_OK)
 
@@ -100,9 +107,10 @@ def search_icon(program_name: str, program_id: str) -> HttpResponse:
                 if extraction_response.status_code in [status.HTTP_200_OK]:
                     return extraction_response  # Successfully found and extracted, or not found but processed
                 else:
-                    # If the extraction failed, log and attempt the next site
                     logger.error("Error during extraction, attempting next site if available.")
 
+    execution_time = time.time() - start_time
+    logger.info(f"Extract icon execution time for {program_name}: {execution_time} seconds.")
     return JsonResponse({'error': f"No links found across all sites for {program_name}"}, status=status.HTTP_200_OK)
 
 
@@ -112,6 +120,7 @@ class IconViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'])
     def download_icon(self, request):
+        start_time = time.time()
 
         """
         Custom action to fetch or search for an icon based on `program_name` and `program_id` from POST data.
