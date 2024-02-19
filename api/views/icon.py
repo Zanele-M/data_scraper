fec62254844497e81199ea987af076bb44fea74f
Zanele Sibanyoni <zs@winfuture.de>
2024-02-19 10:43:04 +0200

    Increase MAX_ATTEMPTS value in icon.py

--- api/views/icon.py
+++ api/views/icon.py
@@ -22,7 +22,7 @@ from api.utils.html_content_parser import extract_html_element_attribute, downlo
 
 logger = logging.getLogger(__name__)
 
-MAX_ATTEMPTS = 100
+MAX_ATTEMPTS = 10000
 
 def extract_icon(url: str, search_term_instance: SearchTerm, program_name: str) -> HttpResponse | Response:
     """
