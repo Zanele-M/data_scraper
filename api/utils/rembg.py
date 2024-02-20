9751e51cd70c39f939dc8b2d090a0c78948e2d9d
Zanele Sibanyoni <zs@winfuture.de>
2024-02-20 14:50:55 +0200

    Modify icon background processing conditions

--- api/utils/rembg.py
+++ api/utils/rembg.py
@@ -11,17 +11,17 @@ def rembg(temp_file_path):
         return None
 
 
-def has_transparent_background(icon):
+def has_transparent_background(icon, program_name):
     if icon.mode == 'RGBA' or icon.mode == 'LA':
-        print("Checking transparency in RGBA or LA mode.")
+        print(f"Checking transparency in RGBA or LA mode for {program_name}.")
         transparent = any(pixel[3] < 255 for pixel in icon.getdata())
         return transparent
     elif icon.mode == 'P' and 'transparency' in icon.info:
-        print("Checking transparency in P mode with transparency info.")
+        print(f"Checking transparency in P mode with transparency info for {program_name}.")
         # In mode 'P', we have to look at the transparency info directly.
         # This is a simplified check; you might need to adjust based on how transparency is defined for your images.
         return True
     else:
         # For other modes, we assume no transparency.
-        print(f"No transparency check implemented for mode {icon.mode}.")
+        print(f"No transparency check implemented for mode {icon.mode} for {program_name}.")
         return False
